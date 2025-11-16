4e2ba9a7d893ee0f4b37feab7646a6f7fbee45bb
hudarafif <rafifhudaaditya@gmail.com>
2025-11-16 18:43:50 +0700

    add feature search in kasirdashboard

--- frontend/src/pages/KasirDashboard.jsx
+++ frontend/src/pages/KasirDashboard.jsx
@@ -6,19 +6,24 @@ const KasirDashboard = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [searchItem, setSearchItem] = useState('');
+
     const { token } = useAuth(); // Ambil token untuk otorisasi
+    // Ambil URL API dari .env
+    const API_URL = import.meta.env.VITE_API_BASE_URL;
     
       useEffect(() => {
+        // setLoading(true);
         const fetchProducts = async () => {
           try {
-            // Ambil URL API dari .env
-            const API_URL = import.meta.env.VITE_API_BASE_URL;
-    
             // Panggil API (membutuhkan token otorisasi)
             const response = await axios.get(`${API_URL}/api/products`, {
               headers: {
                 Authorization: `Bearer ${token}`, // Ini penting!
               },
+              params: {
+                search: searchItem,
+              }
             });
             
             setProducts(response.data); // Simpan data produk ke state
@@ -30,9 +35,13 @@ const KasirDashboard = () => {
         };
     
         if (token) {
-          fetchProducts();
+          const delayDebounceFn = setTimeout(() => {
+            fetchProducts();
+          }, 500);
+    
+          return () => clearTimeout(delayDebounceFn);
         }
-      }, [token]); // Jalankan ulang jika token berubah
+      }, [token, searchItem, API_URL]); // Jalankan ulang jika token berubah
     
       // Tampilkan status loading
       if (loading) {
@@ -46,12 +55,19 @@ const KasirDashboard = () => {
     return(
         <>
         <h2>Kasir Dashboard: Daftar Product</h2>
-            <div className="product-list" style={{ display: 'flex', flexWrap: 'wrap', gap: '16px'  }}>
+        <input 
+        type="text"
+        placeholder= "Cari Produk"
+        style={{ width: '100%', padding: '10px', margin: '20px 0', boxSizing: 'border-box' }}
+        value={searchItem}
+        onChange={(e) => setSearchItem(e.target.value)}
+        />
+          <div className="product-list" style={{ display: 'flex', gap: '16px'  }}>
                     {products.length === 0 ? (
                         <p>belum ada produk</p>
                     ) : (
                         products.map((product) => (
-                            <div key={product.id}
+                            <div key={product._id}
                             style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '8px' }}>
                                 <h4>{product.name}</h4>
                                 <p>Harga: Rp {product.price}</p>
@@ -59,7 +75,7 @@ const KasirDashboard = () => {
                             </div>
                         ))
                     )}
-            </div>
+          </div>
         </>
     ); 
 };
